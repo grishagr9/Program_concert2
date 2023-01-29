35773b9092d6a82c20f058b34858fa1a9b6c3c38
grishagr9 <56480294+grishagr9@users.noreply.github.com>
2023-01-29 07:39:00 +0300

    Add files via upload

---
+++ Program_concert/CreatorForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program_concert
+{
+    public partial class CreatorForm : Form
+    {
+        public Track track = new Track();
+        public CreatorForm()
+        {
+            InitializeComponent();
+            checkedList.CheckOnClick = true;
+            
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            Track track = new Track();
+            track.name = nameBox1.Text;
+            track.genre = checkedList.CheckedItems.Cast<string>().ToList();
+            track.time = timeBox2.Text;
+            if (timeBox2.Text == "")
+            {
+                track.time_seconds = 0;
+                track.time = "0:0";
+            }
+
+            else
+            {
+                track.time = Regex.Replace(timeBox2.Text, @":0", $":"); ;
+                track.time_seconds = track.toSeconds(track.time);
+            }
+                
+            this.track = track;
+            nameBox1.Text = "";
+            timeBox2.Text = "";
+        }
+    }
+}
